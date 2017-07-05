1c7496b78f66e8268a76623d7d09fbc254d8d9b0
Efosa Okpugie <efosaokpugie@gmail.com>
2017-07-05 16:17:00 +0100

    app's configuration file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const routes = require("./routes/index");
+
+app.use(express.static("public"));
+app.use("/",routes)
+app.use(bodyparser.urlencoded({extended : false}));
+
+app.set("port",process.env.PORT || 3000);
+
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', '.hbs');
+
+
+app.listen(app.get("port"),()=>{
+	console.log("app running on port"+" "+app.get("port"));
+});
